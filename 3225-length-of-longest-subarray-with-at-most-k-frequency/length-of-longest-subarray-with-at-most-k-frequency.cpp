2dc748a9819b6d1704ae5b168eb863ae15b45706
itsnitu028 <128230467+itsnitu028@users.noreply.github.com>
2026-08-12 18:51:42 +0530

    Time: 58 ms (82.36%) | Memory: 149.3 MB (84.27%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,29 +1,50 @@
 class Solution {
 public:
     int maxSubarrayLength(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        
         int n=nums.size();
-        unordered_map<int,int> mp;
-        int i=0;
-        int j=0;
-        int result=0;
-        int culprit=0;
-        while(j<n){
+        int i = 0;
+        int j = 0;
+        int result = 0;
+        
+        while(j < n) {
+            
             mp[nums[j]]++;
-            if(mp[nums[j]]==k+1){
-               culprit++;
-            }
-            if(culprit>0){
-                 mp[nums[i]]--;
-                if(mp[nums[i]]==k){
-                culprit--;
-                }
+            
+            while(i < j && mp[nums[j]] > k) {
+                mp[nums[i]]--;
                 i++;
             }
-            if(culprit==0){
-                result=max(result,j-i+1);
-            }
+            
+            result = max(result, j - i + 1);
             j++;
         }
+        
         return result;
+        // int n=nums.size();
+        // unordered_map<int,int> mp;
+        // int i=0;
+        // int j=0;
+        // int result=0;
+        // int culprit=0;
+        // while(j<n){
+        //     mp[nums[j]]++;
+        //     if(mp[nums[j]]==k+1){
+        //        culprit++;
+        //     }
+        //     if(culprit>0){
+        //          mp[nums[i]]--;
+        //         if(mp[nums[i]]==k){
+        //         culprit--;
+        //         }
+        //         i++;
+        //     }
+        //     if(culprit==0){
+        //         result=max(result,j-i+1);
+        //     }
+        //     j++;
+        // }
+        // return result;
     }
 };
